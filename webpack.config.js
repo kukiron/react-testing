2308f5380f3e9d95323c0af708e7372524959297
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2017-10-14 19:44:37 +0600

    Remove comments

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  // path of the main file that will be compiled and output by Webpack.
-  // For React apps, this will usually be the file that mounts the React components onto the DOM.
   entry: ["./src/index.jsx"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
   module: {
     loaders: [
       {
-        // use babel to compile .js and .jsx files using es2015 and react/jsx into regular JavaScript
-        // \.jsx? is a Regular Expression that will match any files with ".js" or ".jsx" in its filename
         test: /\.jsx?/,
         exclude: /node_modules/,
         loader: "babel"
@@ -17,15 +18,6 @@ module.exports = {
     ]
   },
   resolve: {
-    // resolve extensions below, without specifying file extension in your code
-    // ...instead of require('./abc.jsx');
-    // ...we can write require('./abc');
     extensions: ["", ".js", ".jsx"]
-  },
-  output: {
-    // path of the file after it has been compiled
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "bundle.js"
   }
 };
